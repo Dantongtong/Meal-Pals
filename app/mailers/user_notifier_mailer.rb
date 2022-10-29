d8b3787ea974f0c199900fb7edc99e2d816ce65d
Buyi Cheng <bc2996@columbia.edu>
2022-10-29 15:51:29 -0400

    modify from email

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,5 +1,5 @@
 class UserNotifierMailer < ApplicationMailer
-  default :from => 'saasmealpal@gmail.com'
+  default :from => ENV['FROM_MAIL']
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
