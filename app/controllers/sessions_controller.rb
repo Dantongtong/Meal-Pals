8b10c8cbae7099f73f0271087c76b9412a898274
Buyi Cheng <bc2996@columbia.edu>
2022-10-29 15:41:28 -0400

    sign up email confirmation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,19 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email], password: params[:password])
     if user
-      session[:user_id] = user.id
-      flash[:notice] = "Sign in successfully!"
-      redirect_to root_path
+      if user.email_confirmed
+        session[:user_id] = user.id
+        flash[:notice] = "Sign in successfully!"
+        redirect_to root_path
+      else
+        flash[:notice] = 'Please activate your account by following the
+        instructions in the account confirmation email you received to proceed'
+        render action: :new
+      end
     else
       flash[:notice] = "Email or password is incorrect."
       render action: :new
     end
   end
+
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    # Create the user from params
+    @user = User.new(user_params)
+    if @user.save
+      # Deliver the signup email
+      UserNotifierMailer.send_signup_email(@user).deliver
+      flash[:notice] = 'Please confirm your email address to continue'
+      redirect_to new_session_path
+    else
+      render :action => 'new'
+    end
+  end
+
+  def confirm_email
+    user = User.find_by_confirm_token(params[:id])
+    if user
+      user.email_activate
+      flash[:success] = "Welcome to Meal Pal! Your email has been confirmed.
+      Please sign in to continue."
+      redirect_to new_session_path
+    else
+      flash[:error] = "Sorry. User does not exist"
+      redirect_to new_session_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :email, :password)
+  end
+end
