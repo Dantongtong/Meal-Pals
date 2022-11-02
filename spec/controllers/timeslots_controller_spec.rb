0f618d82901b0dec65cc44325082d36a970ef43a
Buyi Cheng <bc2996@columbia.edu>
2022-11-02 08:47:38 -0400

    fix bug

--- spec/controllers/timeslots_controller_spec.rb
+++ spec/controllers/timeslots_controller_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe TimeslotsController, type: :controller do
   describe "show" do
     it "join a timeslot" do    
     @timeslot = Timeslot.create!(restaurant_id: 1, owner: 1, guest: nil, start_time: '10-Nov-2022 12:00:00')
-    get :show, {:id => @timeslot.id, params:{:status => 'active'}, :session => {:user_id => 1} }
+    request.session[:user_id] = 1
+    get :show, {:id => @timeslot.id, :status => 'active'}
     @timeslot.reload
     expect(@timeslot.guest).to eql 1
     end
