612ea9cfef2afedf8d8bb3cf7bbc2b5c212e725c
Huili Zheng <hz2710@cumc.columbia.edu>
2022-11-08 22:52:08 -0500

    updated ownerid to userid

--- spec/controllers/timeslots_controller_spec.rb
+++ spec/controllers/timeslots_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe TimeslotsController, type: :controller do
     
   describe "show" do
     it "join a timeslot" do    
-    @timeslot = Timeslot.create!(restaurant_id: 1, owner: 1, guest: nil, start_time: '10-Nov-2022 12:00:00')
+    @timeslot = Timeslot.create!(restaurant_id: 1, user_id: 1, guest: nil, start_time: '10-Nov-2022 12:00:00')
     request.session[:user_id] = 1
     get :show, {:id => @timeslot.id, :status => 'active'}
     @timeslot.reload
@@ -12,7 +12,7 @@ RSpec.describe TimeslotsController, type: :controller do
     end
 
     it "exit a timeslot" do
-      @timeslot = Timeslot.create!(restaurant_id: 1, owner: 1, guest: 1, start_time: '10-Nov-2022 12:00:00')
+      @timeslot = Timeslot.create!(restaurant_id: 1, user_id: 1, guest: 1, start_time: '10-Nov-2022 12:00:00')
       request.session[:user_id] = 1
       get :show, {:id => @timeslot.id, :status => nil}
       @timeslot.reload
