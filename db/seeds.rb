ec5af6665a513819e4f22749afe00c0035ffcd29
yunyuntian <yurenalexdong@gmail.com>
2022-12-04 12:57:45 -0500

    Added details for restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # add user test data
-User.create([{ email: 'test@columbia.edu', password: '123', username: 'test', email_confirmed: true }])
-User.create([{ email: 'test1@columbia.edu', password: '123', username: 'test1', email_confirmed: true }])
+User.create([{ email: 'test@columbia.edu', password: '123', username: 'John', email_confirmed: true }])
+User.create([{ email: 'test1@columbia.edu', password: '123', username: 'Stacy', email_confirmed: true }])
 User.create([{ email: 'admin@columbia.edu', password: '123', username: 'admin', email_confirmed: true, is_admin: true }])
 
 
 restaurants = [
-  {name: 'Shake Shack', rating: 4, address: 'Unkown', details: 'Unknown'},
-  {name: 'J&J', rating: 3, address: 'Unkown', details: 'Unknown'},
-  {name: 'Dun Huang', rating: 3, address: 'Unkown', details: 'Unknown'},
-  {name: 'Sweetgreen', rating: 3, address: 'Unkown', details: 'Unknown'},
+  {name: 'Shake Shack', rating: 4, address: '2957 Broadway', details: "Shake Shack Inc (Shake Shack) is a fast-food restaurant operator. The company's menu includes burgers, chicken bites, crinkle-cut fries, hot dogs, shakes and frozen custard, Abita root beer, shack-made lemonade, organic fresh brewed iced tea, and organic apple juice."},
+  {name: "JJ's Place", rating: 3, address: 'lower level of John Jay Hall', details: "JJ's Place is the casual and comfortable weekend and late-night destination! Featuring an Angus Steak Burger and other specialty sandwiches plus fries, wings, smoothies, and more."},
+  {name: 'Dun Huang', rating: 3, address: '1268 Amsterdam Ave', details: "As No. 1 hand pulled noodles dish in the city, Dunhuang has the most authentic northwest Chinese cuisine. We are famous for our proud Lanzhou hand pulled beef noodles, fantastic lamb meal and BBQ."},
+  {name: 'Sweetgreen', rating: 3, address: '2937 Broadway', details: 'Fresh salads, plates + grain bowls available for pickup and delivery every day, made in-house from scratch, using whole produce delivered that morning.'},
 ]
 
 restaurants.each do |restaurant|
